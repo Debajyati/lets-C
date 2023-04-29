b5746ab1509d1303e83638d8d542ad07753553c5
Debajyati <ddebajyati@gmail.com>
2023-04-29 12:17:03 +0530

    Adding a file

---
+++ Checking_even_UsingBool.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#include <stdbool.h>
+
+bool is_even(int num) {
+    return num%2==0;
+}
+
+/*Using booleans to verify if a number is even or odd*/
+int main() {
+    int n;
+    printf("Enter a number to check if it\'s even or not\n");
+    scanf("%d",&n);
+    
+    printf("it is %s that %d is even",is_even(n)?"true":"false",n);
+    
+    return 0;
+}
